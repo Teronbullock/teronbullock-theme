cd959bfe21263eee7d6479aaceaadef378ddb87a
Teron Bullock <teron.bullock@gmail.com>
2024-08-26 10:33:07 -0400

    update patterns

--- patterns/project-page-setup.php
+++ patterns/project-page-setup.php
@@ -9,12 +9,16 @@
 <!-- wp:group {"align":"full","layout":{"type":"constrained","contentSize":"900px","wideSize":"1320px"}} -->
 <div class="wp-block-group alignfull"><!-- wp:columns {"align":"wide"} -->
 <div class="wp-block-columns alignwide"><!-- wp:column -->
-<div class="wp-block-column"><!-- wp:paragraph {"className":"entry-tech-stack"} -->
-<p class="entry-tech-stack"><strong>Tech Stack:</strong> Enter skill tags here. ex PHP, HTML, ...</p>
+<div class="wp-block-column"><!-- wp:paragraph {"className":"entry-tech-stack","fontSize":"medium"} -->
+<p class="entry-tech-stack has-medium-font-size"><strong>Tech Stack:</strong> Enter skill tags here. ex PHP, HTML, ...</p>
 <!-- /wp:paragraph -->
 
+<!-- wp:spacer {"height":"0.5rem"} -->
+<div style="height:0.5rem" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
 <!-- wp:buttons -->
-<div class="wp-block-buttons"><!-- wp:button {"textColor":"red","style":{"elements":{"link":{"color":{"text":"var:preset|color|red"}}}},"className":"is-style-outline"} -->
+<div class="wp-block-buttons"><!-- wp:button {"textColor":"red","className":"is-style-outline","style":{"elements":{"link":{"color":{"text":"var:preset|color|red"}}}}} -->
 <div class="wp-block-button is-style-outline"><a class="wp-block-button__link has-red-color has-text-color has-link-color wp-element-button"> Visit Site </a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons -->
@@ -34,7 +38,7 @@
 <!-- /wp:paragraph -->
 
 <!-- wp:paragraph -->
-<p> </p>
+<p></p>
 <!-- /wp:paragraph --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns -->
@@ -45,19 +49,19 @@
 
 <!-- wp:columns {"verticalAlignment":"top","align":"wide","style":{"spacing":{"blockGap":{"left":"1rem"}}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"right":"0px","left":"0px"},"blockGap":"0"}}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0px;padding-left:0px"><!-- wp:image {"align":"center","lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black","className":"is-style-img\u002d\u002dproject"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0px;padding-left:0px"><!-- wp:image {"lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","align":"center","className":"is-style-img\u002d\u002dproject","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black"} -->
 <figure class="wp-block-image aligncenter size-full has-custom-border is-style-img--project"><a href="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png"><img src="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png" alt="" class="has-border-color has-black-border-color wp-image-788" style="border-width:2px;border-radius:8px;aspect-ratio:4/3;object-fit:contain"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"right":"0rem","left":"0rem"}}}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0rem;padding-left:0rem"><!-- wp:image {"align":"center","lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black","className":"is-style-img\u002d\u002dproject"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0rem;padding-left:0rem"><!-- wp:image {"lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","align":"center","className":"is-style-img\u002d\u002dproject","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black"} -->
 <figure class="wp-block-image aligncenter size-full has-custom-border is-style-img--project"><a href="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png"><img src="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png" alt="" class="has-border-color has-black-border-color wp-image-788" style="border-width:2px;border-radius:8px;aspect-ratio:4/3;object-fit:contain"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"right":"0px","left":"0px"},"blockGap":"0"}}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0px;padding-left:0px"><!-- wp:image {"align":"center","lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black","className":"is-style-img\u002d\u002dproject"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0px;padding-left:0px"><!-- wp:image {"lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","align":"center","className":"is-style-img\u002d\u002dproject","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black"} -->
 <figure class="wp-block-image aligncenter size-full has-custom-border is-style-img--project"><a href="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png"><img src="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png" alt="" class="has-border-color has-black-border-color wp-image-788" style="border-width:2px;border-radius:8px;aspect-ratio:4/3;object-fit:contain"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
@@ -65,19 +69,19 @@
 
 <!-- wp:columns {"verticalAlignment":"top","align":"wide","style":{"spacing":{"blockGap":{"left":"1rem"}}}} -->
 <div class="wp-block-columns alignwide are-vertically-aligned-top"><!-- wp:column {"verticalAlignment":"top"} -->
-<div class="wp-block-column is-vertically-aligned-top"><!-- wp:image {"align":"center","lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black","className":"is-style-img\u002d\u002dproject"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:image {"lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","align":"center","className":"is-style-img\u002d\u002dproject","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black"} -->
 <figure class="wp-block-image aligncenter size-full has-custom-border is-style-img--project"><a href="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png"><img src="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png" alt="" class="has-border-color has-black-border-color wp-image-788" style="border-width:2px;border-radius:8px;aspect-ratio:4/3;object-fit:contain"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"top","style":{"spacing":{"padding":{"right":"0rem","left":"0rem"}}}} -->
-<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0rem;padding-left:0rem"><!-- wp:image {"align":"center","lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black","className":"is-style-img\u002d\u002dproject"} -->
+<div class="wp-block-column is-vertically-aligned-top" style="padding-right:0rem;padding-left:0rem"><!-- wp:image {"lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","align":"center","className":"is-style-img\u002d\u002dproject","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black"} -->
 <figure class="wp-block-image aligncenter size-full has-custom-border is-style-img--project"><a href="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png"><img src="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png" alt="" class="has-border-color has-black-border-color wp-image-788" style="border-width:2px;border-radius:8px;aspect-ratio:4/3;object-fit:contain"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column -->
 
 <!-- wp:column {"verticalAlignment":"top"} -->
-<div class="wp-block-column is-vertically-aligned-top"><!-- wp:image {"align":"center","lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black","className":"is-style-img\u002d\u002dproject"} -->
+<div class="wp-block-column is-vertically-aligned-top"><!-- wp:image {"lightbox":{"enabled":false},"id":788,"aspectRatio":"4/3","scale":"contain","sizeSlug":"full","linkDestination":"media","align":"center","className":"is-style-img\u002d\u002dproject","style":{"border":{"radius":"8px","width":"2px"}},"borderColor":"black"} -->
 <figure class="wp-block-image aligncenter size-full has-custom-border is-style-img--project"><a href="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png"><img src="http://teronbullock.local/wp-content/uploads/2024/01/dummy_418x350_ffffff_cccccc_site-image.png" alt="" class="has-border-color has-black-border-color wp-image-788" style="border-width:2px;border-radius:8px;aspect-ratio:4/3;object-fit:contain"/></a></figure>
 <!-- /wp:image --></div>
 <!-- /wp:column --></div>
