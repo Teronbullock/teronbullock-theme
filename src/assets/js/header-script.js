c77cfca4a237eb77c4016455788c2b9228116400
Teron Bullock <teron.bullock@gmail.com>
2025-08-20 11:23:49 -0400

    fix header style issue

--- src/assets/js/header-script.js
+++ src/assets/js/header-script.js
@@ -2,25 +2,25 @@
 const header = document.querySelector('[data-js="header"]');
 const body = document.querySelector('body');
 
-// Get the offset position of the navbar
-if (
-  header &&
-  body &&
-  (body.classList.contains('home') ||
+if (header && body) {
+  // Get the offset position of the navbar
+  if (
+    body.classList.contains('home') ||
     body.classList.contains(
       'page-template-wp-custom-template-portfolio-pathways'
-    ))
-) {
-  // scroll listener
-  window.addEventListener('scroll', e => {
-    // if scroll is greater than 70px
-    if (window.scrollY >= 70 && window.innerWidth > 769) {
-      // remove class from header
-      header.classList.remove('top');
-    } else {
-      header.classList.add('top');
-    }
-  });
-} else {
-  header.classList.remove('top');
+    )
+  ) {
+    // scroll listener
+    window.addEventListener('scroll', e => {
+      // if scroll is greater than 70px
+      if (window.scrollY >= 70 && window.innerWidth > 769) {
+        // remove class from header
+        header.classList.remove('top');
+      } else {
+        header.classList.add('top');
+      }
+    });
+  } else {
+    header.classList.remove('top');
+  }
 }
